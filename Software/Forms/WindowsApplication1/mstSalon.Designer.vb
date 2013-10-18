2998f3845548d3eb63158f1a58380f2d8e38fa46
estuardohbo <estuardohbo@gmail.com>
2013-10-17 23:58:51 -0600

    1v. Maestro Salon

---
+++ Software/Forms/WindowsApplication1/mstSalon.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class mstSalon
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(mstSalon))
+        Me.dgvSalon = New System.Windows.Forms.DataGridView()
+        Me.gbIngresoDatos = New System.Windows.Forms.GroupBox()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnAniadir = New System.Windows.Forms.Button()
+        Me.txtDescripcion = New System.Windows.Forms.TextBox()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.dgvSalon, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.gbIngresoDatos.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dgvSalon
+        '
+        Me.dgvSalon.AllowUserToAddRows = False
+        Me.dgvSalon.AllowUserToDeleteRows = False
+        Me.dgvSalon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvSalon.Location = New System.Drawing.Point(12, 178)
+        Me.dgvSalon.Name = "dgvSalon"
+        Me.dgvSalon.ReadOnly = True
+        Me.dgvSalon.Size = New System.Drawing.Size(529, 184)
+        Me.dgvSalon.TabIndex = 12
+        '
+        'gbIngresoDatos
+        '
+        Me.gbIngresoDatos.Controls.Add(Me.btnModificar)
+        Me.gbIngresoDatos.Controls.Add(Me.btnEliminar)
+        Me.gbIngresoDatos.Controls.Add(Me.btnLimpiar)
+        Me.gbIngresoDatos.Controls.Add(Me.btnAniadir)
+        Me.gbIngresoDatos.Controls.Add(Me.txtDescripcion)
+        Me.gbIngresoDatos.Controls.Add(Me.txtCantidad)
+        Me.gbIngresoDatos.Controls.Add(Me.txtNombre)
+        Me.gbIngresoDatos.Controls.Add(Me.Label2)
+        Me.gbIngresoDatos.Controls.Add(Me.Label6)
+        Me.gbIngresoDatos.Controls.Add(Me.Label1)
+        Me.gbIngresoDatos.Location = New System.Drawing.Point(12, 12)
+        Me.gbIngresoDatos.Name = "gbIngresoDatos"
+        Me.gbIngresoDatos.Size = New System.Drawing.Size(529, 143)
+        Me.gbIngresoDatos.TabIndex = 11
+        Me.gbIngresoDatos.TabStop = False
+        Me.gbIngresoDatos.Text = "Ingreso de datos"
+        '
+        'btnModificar
+        '
+        Me.btnModificar.Image = CType(resources.GetObject("btnModificar.Image"), System.Drawing.Image)
+        Me.btnModificar.Location = New System.Drawing.Point(478, 19)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(36, 36)
+        Me.btnModificar.TabIndex = 9
+        Me.btnModificar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Image = CType(resources.GetObject("btnEliminar.Image"), System.Drawing.Image)
+        Me.btnEliminar.Location = New System.Drawing.Point(436, 19)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(36, 36)
+        Me.btnEliminar.TabIndex = 8
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Image = CType(resources.GetObject("btnLimpiar.Image"), System.Drawing.Image)
+        Me.btnLimpiar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnLimpiar.Location = New System.Drawing.Point(21, 19)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(86, 36)
+        Me.btnLimpiar.TabIndex = 7
+        Me.btnLimpiar.Text = "          Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'btnAniadir
+        '
+        Me.btnAniadir.Image = CType(resources.GetObject("btnAniadir.Image"), System.Drawing.Image)
+        Me.btnAniadir.Location = New System.Drawing.Point(394, 19)
+        Me.btnAniadir.Name = "btnAniadir"
+        Me.btnAniadir.Size = New System.Drawing.Size(36, 36)
+        Me.btnAniadir.TabIndex = 7
+        Me.btnAniadir.UseVisualStyleBackColor = True
+        '
+        'txtDescripcion
+        '
+        Me.txtDescripcion.Location = New System.Drawing.Point(108, 87)
+        Me.txtDescripcion.MaxLength = 200
+        Me.txtDescripcion.Multiline = True
+        Me.txtDescripcion.Name = "txtDescripcion"
+        Me.txtDescripcion.Size = New System.Drawing.Size(406, 43)
+        Me.txtDescripcion.TabIndex = 6
+        Me.txtDescripcion.Tag = "observacion"
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(108, 60)
+        Me.txtCantidad.MaxLength = 4
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(79, 20)
+        Me.txtCantidad.TabIndex = 1
+        Me.txtCantidad.Tag = "capacidad"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(246, 61)
+        Me.txtNombre.MaxLength = 100
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(268, 20)
+        Me.txtNombre.TabIndex = 2
+        Me.txtNombre.Tag = "nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Descripción:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(18, 63)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 13)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Cantidad"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(193, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nombre:"
+        '
+        'mstSalon
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(561, 376)
+        Me.Controls.Add(Me.dgvSalon)
+        Me.Controls.Add(Me.gbIngresoDatos)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "mstSalon"
+        Me.Text = "Salon"
+        CType(Me.dgvSalon, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.gbIngresoDatos.ResumeLayout(False)
+        Me.gbIngresoDatos.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents dgvSalon As System.Windows.Forms.DataGridView
+    Friend WithEvents gbIngresoDatos As System.Windows.Forms.GroupBox
+    Friend WithEvents btnModificar As System.Windows.Forms.Button
+    Friend WithEvents btnEliminar As System.Windows.Forms.Button
+    Friend WithEvents btnLimpiar As System.Windows.Forms.Button
+    Friend WithEvents btnAniadir As System.Windows.Forms.Button
+    Friend WithEvents txtDescripcion As System.Windows.Forms.TextBox
+    Friend WithEvents txtCantidad As System.Windows.Forms.TextBox
+    Friend WithEvents txtNombre As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+End Class
---
+++ Software/Forms/WindowsApplication1/mstSalon.vb
@@ -0,0 +1,73 @@
+﻿Imports WindowsApplication1.clsGlobales
+Imports WindowsApplication1.clsHelper
+Imports WindowsApplication1.clsHelperSQL
+Imports WindowsApplication1.clsABC
+
+Public Class mstSalon
+
+#Region "variables"
+    Dim iId As String
+    Dim VectorTxt(3) As TextBox
+#End Region
+
+    Private Sub LlenarTextBox(incluirID As Boolean)
+        VectorTxt(0) = txtCantidad
+        VectorTxt(1) = txtNombre
+        VectorTxt(2) = txtDescripcion
+
+        VectorTxt(3) = New TextBox
+        VectorTxt(3).Tag = "id_salon" ''nombre bd
+        If incluirID Then
+            VectorTxt(3).Text = Guid.NewGuid.ToString
+        Else
+            VectorTxt(3).Text = iId
+        End If
+
+    End Sub
+
+
+    Private Sub mstSalon_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ''grid
+        FillDataGridView(dgvSalon, ExecuteQuery("Select * from salon"))
+        ''dgvCarrera.Columns("sucursal_id_sucursal").Visible = False
+    End Sub
+
+    Private Sub btnAniadir_Click(sender As Object, e As EventArgs) Handles btnAniadir.Click
+        LlenarTextBox(True)
+        Dim sError As String = SaveData("salon", VectorTxt)
+        If (sError = "") Then
+            FillDataGridView(dgvSalon, ExecuteQuery("Select * from salon"))
+            LimpiarTxtBox(Me)
+        End If
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Dim sError As String = DeleteData("salon", "id_salon = '" & iId & "'")
+        If (sError = "") Then
+            FillDataGridView(dgvSalon, ExecuteQuery("Select * from salon"))
+            LimpiarTxtBox(Me)
+        End If
+    End Sub
+
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        LlenarTextBox(False)
+        Dim sError As String = UpdateData("salon", VectorTxt, "id_salon = '" & iId & "'")
+        If (sError = "") Then
+            FillDataGridView(dgvSalon, ExecuteQuery("Select * from salon"))
+            LimpiarTxtBox(Me)
+        End If
+    End Sub
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        LimpiarTxtBox(Me)
+    End Sub
+
+    Private Sub dgvSalon_SelectionChanged(sender As Object, e As EventArgs) Handles dgvSalon.SelectionChanged
+        iId = GetItem(dgvSalon, "id_salon")
+
+        txtNombre.Text = GetItem(dgvSalon, "nombre")
+        txtDescripcion.Text = GetItem(dgvSalon, "observacion")
+        txtCantidad.Text = GetItem(dgvSalon, "capacidad") ''nombre de la bd
+       
+    End Sub
+End Class
