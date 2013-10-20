2bb632c96c538fbd0179a5ba3f60053382e99c67
Balam <bodriguezscg@gmail.com>
2013-10-20 16:36:41 -0600

    continuando con el desarrollo de la asignacion de catedratico

--- Software/Forms/WindowsApplication1/TrsAsignacionCatedratico.Designer.vb
+++ Software/Forms/WindowsApplication1/TrsAsignacionCatedratico.Designer.vb
@@ -25,31 +25,33 @@ Partial Class TrsAsignacionCatedratico
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(TrsAsignacionCatedratico))
         Me.dgvCatedras = New System.Windows.Forms.DataGridView()
         Me.gbIngresoDatos = New System.Windows.Forms.GroupBox()
+        Me.cbSeccion = New System.Windows.Forms.ComboBox()
+        Me.cbSalon = New System.Windows.Forms.ComboBox()
+        Me.cbHorario = New System.Windows.Forms.ComboBox()
+        Me.cbCatedratico = New System.Windows.Forms.ComboBox()
         Me.btnModificar = New System.Windows.Forms.Button()
         Me.btnEliminar = New System.Windows.Forms.Button()
         Me.btnAniadir = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
-        Me.bgBusquedaSeccion = New System.Windows.Forms.GroupBox()
-        Me.ComboBox5 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox6 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox7 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox8 = New System.Windows.Forms.ComboBox()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.bgBusquedaCatedra = New System.Windows.Forms.GroupBox()
+        Me.cbJornada = New System.Windows.Forms.ComboBox()
+        Me.cbCarrera = New System.Windows.Forms.ComboBox()
+        Me.cbCiclo = New System.Windows.Forms.ComboBox()
+        Me.cbAnio = New System.Windows.Forms.ComboBox()
+        Me.cbCatedra = New System.Windows.Forms.ComboBox()
         Me.Label9 = New System.Windows.Forms.Label()
-        Me.ComboBox9 = New System.Windows.Forms.ComboBox()
+        Me.cbPensum = New System.Windows.Forms.ComboBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
         CType(Me.dgvCatedras, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.gbIngresoDatos.SuspendLayout()
-        Me.bgBusquedaSeccion.SuspendLayout()
+        Me.bgBusquedaCatedra.SuspendLayout()
         Me.SuspendLayout()
         '
         'dgvCatedras
@@ -57,18 +59,18 @@ Partial Class TrsAsignacionCatedratico
         Me.dgvCatedras.AllowUserToAddRows = False
         Me.dgvCatedras.AllowUserToDeleteRows = False
         Me.dgvCatedras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvCatedras.Location = New System.Drawing.Point(28, 270)
+        Me.dgvCatedras.Location = New System.Drawing.Point(12, 215)
         Me.dgvCatedras.Name = "dgvCatedras"
         Me.dgvCatedras.ReadOnly = True
-        Me.dgvCatedras.Size = New System.Drawing.Size(529, 146)
+        Me.dgvCatedras.Size = New System.Drawing.Size(529, 204)
         Me.dgvCatedras.TabIndex = 18
         '
         'gbIngresoDatos
         '
-        Me.gbIngresoDatos.Controls.Add(Me.ComboBox4)
-        Me.gbIngresoDatos.Controls.Add(Me.ComboBox3)
-        Me.gbIngresoDatos.Controls.Add(Me.ComboBox2)
-        Me.gbIngresoDatos.Controls.Add(Me.ComboBox1)
+        Me.gbIngresoDatos.Controls.Add(Me.cbSeccion)
+        Me.gbIngresoDatos.Controls.Add(Me.cbSalon)
+        Me.gbIngresoDatos.Controls.Add(Me.cbHorario)
+        Me.gbIngresoDatos.Controls.Add(Me.cbCatedratico)
         Me.gbIngresoDatos.Controls.Add(Me.btnModificar)
         Me.gbIngresoDatos.Controls.Add(Me.btnEliminar)
         Me.gbIngresoDatos.Controls.Add(Me.btnAniadir)
@@ -76,12 +78,44 @@ Partial Class TrsAsignacionCatedratico
         Me.gbIngresoDatos.Controls.Add(Me.Label6)
         Me.gbIngresoDatos.Controls.Add(Me.Label3)
         Me.gbIngresoDatos.Controls.Add(Me.Label1)
-        Me.gbIngresoDatos.Location = New System.Drawing.Point(28, 146)
+        Me.gbIngresoDatos.Location = New System.Drawing.Point(12, 85)
         Me.gbIngresoDatos.Name = "gbIngresoDatos"
-        Me.gbIngresoDatos.Size = New System.Drawing.Size(529, 118)
+        Me.gbIngresoDatos.Size = New System.Drawing.Size(529, 124)
         Me.gbIngresoDatos.TabIndex = 17
         Me.gbIngresoDatos.TabStop = False
-        Me.gbIngresoDatos.Text = "Ingreso de datos"
+        Me.gbIngresoDatos.Text = "Asignacion de Catedratico para la Catedra "
+        '
+        'cbSeccion
+        '
+        Me.cbSeccion.FormattingEnabled = True
+        Me.cbSeccion.Location = New System.Drawing.Point(73, 61)
+        Me.cbSeccion.Name = "cbSeccion"
+        Me.cbSeccion.Size = New System.Drawing.Size(68, 21)
+        Me.cbSeccion.TabIndex = 11
+        '
+        'cbSalon
+        '
+        Me.cbSalon.FormattingEnabled = True
+        Me.cbSalon.Location = New System.Drawing.Point(73, 88)
+        Me.cbSalon.Name = "cbSalon"
+        Me.cbSalon.Size = New System.Drawing.Size(137, 21)
+        Me.cbSalon.TabIndex = 11
+        '
+        'cbHorario
+        '
+        Me.cbHorario.FormattingEnabled = True
+        Me.cbHorario.Location = New System.Drawing.Point(263, 88)
+        Me.cbHorario.Name = "cbHorario"
+        Me.cbHorario.Size = New System.Drawing.Size(251, 21)
+        Me.cbHorario.TabIndex = 10
+        '
+        'cbCatedratico
+        '
+        Me.cbCatedratico.FormattingEnabled = True
+        Me.cbCatedratico.Location = New System.Drawing.Point(219, 61)
+        Me.cbCatedratico.Name = "cbCatedratico"
+        Me.cbCatedratico.Size = New System.Drawing.Size(295, 21)
+        Me.cbCatedratico.TabIndex = 10
         '
         'btnModificar
         '
@@ -122,20 +156,11 @@ Partial Class TrsAsignacionCatedratico
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(146, 64)
+        Me.Label6.Location = New System.Drawing.Point(156, 64)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(57, 13)
         Me.Label6.TabIndex = 0
         Me.Label6.Text = "Empleado:"
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(18, 91)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(37, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Salon:"
         '
         'Label3
         '
@@ -146,88 +171,101 @@ Partial Class TrsAsignacionCatedratico
         Me.Label3.TabIndex = 0
         Me.Label3.Text = "Horario"
         '
-        'ComboBox1
+        'Label1
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(207, 61)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(307, 21)
-        Me.ComboBox1.TabIndex = 10
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 91)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Salon:"
         '
-        'ComboBox2
+        'bgBusquedaCatedra
         '
-        Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(263, 88)
-        Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(251, 21)
-        Me.ComboBox2.TabIndex = 10
+        Me.bgBusquedaCatedra.Controls.Add(Me.cbJornada)
+        Me.bgBusquedaCatedra.Controls.Add(Me.cbCarrera)
+        Me.bgBusquedaCatedra.Controls.Add(Me.cbCiclo)
+        Me.bgBusquedaCatedra.Controls.Add(Me.cbAnio)
+        Me.bgBusquedaCatedra.Controls.Add(Me.cbCatedra)
+        Me.bgBusquedaCatedra.Controls.Add(Me.Label9)
+        Me.bgBusquedaCatedra.Controls.Add(Me.cbPensum)
+        Me.bgBusquedaCatedra.Controls.Add(Me.Label10)
+        Me.bgBusquedaCatedra.Controls.Add(Me.Label4)
+        Me.bgBusquedaCatedra.Controls.Add(Me.Label7)
+        Me.bgBusquedaCatedra.Controls.Add(Me.Label5)
+        Me.bgBusquedaCatedra.Controls.Add(Me.Label8)
+        Me.bgBusquedaCatedra.Location = New System.Drawing.Point(12, 3)
+        Me.bgBusquedaCatedra.Name = "bgBusquedaCatedra"
+        Me.bgBusquedaCatedra.Size = New System.Drawing.Size(529, 76)
+        Me.bgBusquedaCatedra.TabIndex = 17
+        Me.bgBusquedaCatedra.TabStop = False
+        Me.bgBusquedaCatedra.Text = "Busqueda cátedra"
         '
-        'ComboBox3
+        'cbJornada
         '
-        Me.ComboBox3.FormattingEnabled = True
-        Me.ComboBox3.Location = New System.Drawing.Point(73, 88)
-        Me.ComboBox3.Name = "ComboBox3"
-        Me.ComboBox3.Size = New System.Drawing.Size(137, 21)
-        Me.ComboBox3.TabIndex = 11
+        Me.cbJornada.FormattingEnabled = True
+        Me.cbJornada.Location = New System.Drawing.Point(263, 19)
+        Me.cbJornada.Name = "cbJornada"
+        Me.cbJornada.Size = New System.Drawing.Size(99, 21)
+        Me.cbJornada.TabIndex = 11
         '
-        'ComboBox4
+        'cbCarrera
         '
-        Me.ComboBox4.FormattingEnabled = True
-        Me.ComboBox4.Location = New System.Drawing.Point(73, 61)
-        Me.ComboBox4.Name = "ComboBox4"
-        Me.ComboBox4.Size = New System.Drawing.Size(68, 21)
-        Me.ComboBox4.TabIndex = 11
+        Me.cbCarrera.FormattingEnabled = True
+        Me.cbCarrera.Location = New System.Drawing.Point(66, 19)
+        Me.cbCarrera.Name = "cbCarrera"
+        Me.cbCarrera.Size = New System.Drawing.Size(137, 21)
+        Me.cbCarrera.TabIndex = 11
         '
-        'bgBusquedaSeccion
+        'cbCiclo
         '
-        Me.bgBusquedaSeccion.Controls.Add(Me.ComboBox9)
-        Me.bgBusquedaSeccion.Controls.Add(Me.ComboBox5)
-        Me.bgBusquedaSeccion.Controls.Add(Me.ComboBox6)
-        Me.bgBusquedaSeccion.Controls.Add(Me.ComboBox7)
-        Me.bgBusquedaSeccion.Controls.Add(Me.Label9)
-        Me.bgBusquedaSeccion.Controls.Add(Me.ComboBox8)
-        Me.bgBusquedaSeccion.Controls.Add(Me.Label4)
-        Me.bgBusquedaSeccion.Controls.Add(Me.Label5)
-        Me.bgBusquedaSeccion.Controls.Add(Me.Label7)
-        Me.bgBusquedaSeccion.Controls.Add(Me.Label8)
-        Me.bgBusquedaSeccion.Location = New System.Drawing.Point(28, 3)
-        Me.bgBusquedaSeccion.Name = "bgBusquedaSeccion"
-        Me.bgBusquedaSeccion.Size = New System.Drawing.Size(529, 118)
-        Me.bgBusquedaSeccion.TabIndex = 17
-        Me.bgBusquedaSeccion.TabStop = False
-        Me.bgBusquedaSeccion.Text = "Busqueda Seccion"
+        Me.cbCiclo.FormattingEnabled = True
+        Me.cbCiclo.Location = New System.Drawing.Point(422, 46)
+        Me.cbCiclo.Name = "cbCiclo"
+        Me.cbCiclo.Size = New System.Drawing.Size(92, 21)
+        Me.cbCiclo.TabIndex = 11
         '
-        'ComboBox5
+        'cbAnio
         '
-        Me.ComboBox5.FormattingEnabled = True
-        Me.ComboBox5.Location = New System.Drawing.Point(73, 19)
-        Me.ComboBox5.Name = "ComboBox5"
-        Me.ComboBox5.Size = New System.Drawing.Size(68, 21)
-        Me.ComboBox5.TabIndex = 11
+        Me.cbAnio.FormattingEnabled = True
+        Me.cbAnio.Location = New System.Drawing.Point(260, 46)
+        Me.cbAnio.Name = "cbAnio"
+        Me.cbAnio.Size = New System.Drawing.Size(102, 21)
+        Me.cbAnio.TabIndex = 11
         '
-        'ComboBox6
+        'cbCatedra
         '
-        Me.ComboBox6.FormattingEnabled = True
-        Me.ComboBox6.Location = New System.Drawing.Point(73, 46)
-        Me.ComboBox6.Name = "ComboBox6"
-        Me.ComboBox6.Size = New System.Drawing.Size(137, 21)
-        Me.ComboBox6.TabIndex = 11
+        Me.cbCatedra.FormattingEnabled = True
+        Me.cbCatedra.Location = New System.Drawing.Point(66, 46)
+        Me.cbCatedra.Name = "cbCatedra"
+        Me.cbCatedra.Size = New System.Drawing.Size(137, 21)
+        Me.cbCatedra.TabIndex = 11
         '
-        'ComboBox7
+        'Label9
         '
-        Me.ComboBox7.FormattingEnabled = True
-        Me.ComboBox7.Location = New System.Drawing.Point(263, 46)
-        Me.ComboBox7.Name = "ComboBox7"
-        Me.ComboBox7.Size = New System.Drawing.Size(251, 21)
-        Me.ComboBox7.TabIndex = 10
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(209, 22)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(48, 13)
+        Me.Label9.TabIndex = 0
+        Me.Label9.Text = "Jornada:"
         '
-        'ComboBox8
+        'cbPensum
         '
-        Me.ComboBox8.FormattingEnabled = True
-        Me.ComboBox8.Location = New System.Drawing.Point(366, 19)
-        Me.ComboBox8.Name = "ComboBox8"
-        Me.ComboBox8.Size = New System.Drawing.Size(148, 21)
-        Me.ComboBox8.TabIndex = 10
+        Me.cbPensum.FormattingEnabled = True
+        Me.cbPensum.Location = New System.Drawing.Point(422, 19)
+        Me.cbPensum.Name = "cbPensum"
+        Me.cbPensum.Size = New System.Drawing.Size(92, 21)
+        Me.cbPensum.TabIndex = 10
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(368, 49)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(33, 13)
+        Me.Label10.TabIndex = 0
+        Me.Label10.Text = "Ciclo:"
         '
         'Label4
         '
@@ -238,23 +276,23 @@ Partial Class TrsAsignacionCatedratico
         Me.Label4.TabIndex = 0
         Me.Label4.Text = "Carrera:"
         '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(209, 49)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(29, 13)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "Año:"
+        '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(312, 22)
+        Me.Label5.Location = New System.Drawing.Point(368, 22)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(48, 13)
         Me.Label5.TabIndex = 0
         Me.Label5.Text = "Pensum:"
-        '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(216, 49)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(41, 13)
-        Me.Label7.TabIndex = 0
-        Me.Label7.Text = "Horario"
         '
         'Label8
         '
@@ -265,30 +303,13 @@ Partial Class TrsAsignacionCatedratico
         Me.Label8.TabIndex = 0
         Me.Label8.Text = "Catedra:"
         '
-        'Label9
-        '
-        Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(152, 22)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(44, 13)
-        Me.Label9.TabIndex = 0
-        Me.Label9.Text = "Carrera:"
-        '
-        'ComboBox9
-        '
-        Me.ComboBox9.FormattingEnabled = True
-        Me.ComboBox9.Location = New System.Drawing.Point(207, 19)
-        Me.ComboBox9.Name = "ComboBox9"
-        Me.ComboBox9.Size = New System.Drawing.Size(99, 21)
-        Me.ComboBox9.TabIndex = 11
-        '
         'TrsAsignacionCatedratico
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(585, 417)
+        Me.ClientSize = New System.Drawing.Size(557, 430)
         Me.Controls.Add(Me.dgvCatedras)
-        Me.Controls.Add(Me.bgBusquedaSeccion)
+        Me.Controls.Add(Me.bgBusquedaCatedra)
         Me.Controls.Add(Me.gbIngresoDatos)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "TrsAsignacionCatedratico"
@@ -296,17 +317,17 @@ Partial Class TrsAsignacionCatedratico
         CType(Me.dgvCatedras, System.ComponentModel.ISupportInitialize).EndInit()
         Me.gbIngresoDatos.ResumeLayout(False)
         Me.gbIngresoDatos.PerformLayout()
-        Me.bgBusquedaSeccion.ResumeLayout(False)
-        Me.bgBusquedaSeccion.PerformLayout()
+        Me.bgBusquedaCatedra.ResumeLayout(False)
+        Me.bgBusquedaCatedra.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents dgvCatedras As System.Windows.Forms.DataGridView
     Friend WithEvents gbIngresoDatos As System.Windows.Forms.GroupBox
-    Friend WithEvents ComboBox4 As System.Windows.Forms.ComboBox
-    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
-    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
-    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents cbSeccion As System.Windows.Forms.ComboBox
+    Friend WithEvents cbSalon As System.Windows.Forms.ComboBox
+    Friend WithEvents cbHorario As System.Windows.Forms.ComboBox
+    Friend WithEvents cbCatedratico As System.Windows.Forms.ComboBox
     Friend WithEvents btnModificar As System.Windows.Forms.Button
     Friend WithEvents btnEliminar As System.Windows.Forms.Button
     Friend WithEvents btnAniadir As System.Windows.Forms.Button
@@ -314,15 +335,17 @@ Partial Class TrsAsignacionCatedratico
     Friend WithEvents Label6 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents bgBusquedaSeccion As System.Windows.Forms.GroupBox
-    Friend WithEvents ComboBox9 As System.Windows.Forms.ComboBox
-    Friend WithEvents ComboBox5 As System.Windows.Forms.ComboBox
-    Friend WithEvents ComboBox6 As System.Windows.Forms.ComboBox
-    Friend WithEvents ComboBox7 As System.Windows.Forms.ComboBox
+    Friend WithEvents bgBusquedaCatedra As System.Windows.Forms.GroupBox
+    Friend WithEvents cbJornada As System.Windows.Forms.ComboBox
+    Friend WithEvents cbCarrera As System.Windows.Forms.ComboBox
+    Friend WithEvents cbCatedra As System.Windows.Forms.ComboBox
     Friend WithEvents Label9 As System.Windows.Forms.Label
-    Friend WithEvents ComboBox8 As System.Windows.Forms.ComboBox
+    Friend WithEvents cbPensum As System.Windows.Forms.ComboBox
     Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents Label5 As System.Windows.Forms.Label
-    Friend WithEvents Label7 As System.Windows.Forms.Label
     Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents cbAnio As System.Windows.Forms.ComboBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents cbCiclo As System.Windows.Forms.ComboBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
 End Class
--- Software/Forms/WindowsApplication1/TrsAsignacionCatedratico.vb
+++ Software/Forms/WindowsApplication1/TrsAsignacionCatedratico.vb
@@ -1,6 +1,58 @@
-﻿Public Class TrsAsignacionCatedratico
+﻿Imports WindowsApplication1.clsABC
+Imports WindowsApplication1.clsHelperSQL
+Imports WindowsApplication1.clsHelper
+Imports WindowsApplication1.clsGlobales
 
-    Private Sub ComboBox4_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox4.SelectedIndexChanged, ComboBox9.SelectedIndexChanged, ComboBox5.SelectedIndexChanged
+Public Class TrsAsignacionCatedratico
+    Private Sub TrsAsignacionCatedratico_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim DsComboCarrera As DataSet = ExecuteQuery("select id_carrera as id, nombre from carrera")
+        FillComboBox(cbCarrera, DsComboCarrera, "id", "nombre")
 
+        Dim DsComboCategratico As DataSet = ExecuteQuery("select id_empleado as id,concat(nombres, ' ', apellidos) as nombre from empleado")
+        FillComboBox(cbCatedratico, DsComboCategratico, "id", "nombre")
+
+        Dim DsSalon As DataSet = ExecuteQuery("select id_salon as id, nombre as nombre from salon")
+        FillComboBox(cbSalon, DsSalon, "id", "nombre")
+
+        Dim DsHorario As DataSet = ExecuteQuery("select id_horario as id,concat(hora_inicial, ' - ', hora_final) as nombre from horario")
+        FillComboBox(cbHorario, DsHorario, "id", "nombre")
+
+        If cbCarrera.Text <> "" Then
+            Dim DsComboJornada As DataSet = ExecuteQuery("select id_jornada as id, nombre from jornada where id_carrera = '" & cbCarrera.SelectedValue & "'")
+            FillComboBox(cbJornada, DsComboJornada, "id", "nombre")
+
+            Dim DsPensum As DataSet = ExecuteQuery("select id_pensum as id, descripcion from pensum where carrera_id_carrera = '" & cbCarrera.SelectedValue & "'")
+            FillComboBox(cbPensum, DsPensum, "id", "descripcion")
+
+            Dim DsCatedratico As DataSet = ExecuteQuery("select id_pensum as id, descripcion from pensum where carrera_id_carrera = '" & cbCarrera.SelectedValue & "'")
+            FillComboBox(cbPensum, DsPensum, "id", "descripcion")
+
+            If cbPensum.Text <> "" And cbJornada.Text <> "" Then
+                Dim DsCatedra As DataSet = ExecuteQuery("select id_catedra as id, nombre from catedra where pensum_id_pensum = '" & cbPensum.SelectedValue & "'")
+                FillComboBox(cbCatedra, DsCatedra, "id", "nombre")
+
+                If cbCatedra.Text <> "" Then
+                    gbIngresoDatos.Text = "Asignacion de Catedratico para la Cátedra:" + cbCatedra.Text
+
+                    Dim DsAnio As DataSet = ExecuteQuery("select anio from seccion where catedra_id_catedra = '" & cbCatedra.SelectedValue & "'  group by anio ")
+                    FillComboBox(cbAnio, DsAnio, "anio", "anio")
+
+                    If cbAnio.Text <> "" Then
+                        Dim DsCiclo As DataSet = ExecuteQuery("select ciclo_anual from seccion where catedra_id_catedra = '" & cbCatedra.SelectedValue & "'  group by ciclo_anual")
+                        FillComboBox(cbCiclo, DsCiclo, "ciclo_anual", "ciclo_anual")
+
+                        If cbAnio.Text <> "" Then
+                            Dim DsSeccion As DataSet = ExecuteQuery("select id_seccion as id, nombre from seccion where catedra_id_catedra = '" & cbCatedra.SelectedValue & "' and anio = " & cbAnio.Text & " and ciclo_anual = '" & cbCiclo.Text & "'")
+                            FillComboBox(cbSeccion, DsSeccion, "id", "nombre")
+                        End If
+                    End If
+
+                End If
+            End If
+        End If
+    End Sub
+
+    Private Sub cbJornada_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbJornada.SelectedIndexChanged
+       
     End Sub
 End Class
